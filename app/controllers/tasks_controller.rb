625ce95fa159d18511ed95ad1efe5de9748d23f9
sayaka.haneda <haaaaane051@yahoo.co.jp>
2017-04-25 06:42:41 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,7 +48,7 @@ class TasksController < ApplicationController
   end
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :title)
   end
   
 end
